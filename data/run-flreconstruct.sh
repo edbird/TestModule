41fea451f87e6afebfec956334b6a5f3ebee2ecf
edbird <2939755+edbird@users.noreply.github.com>
2018-01-15 11:04:38 +0000

    Delete run-flreconstruct.sh

--- data/run-flreconstruct.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#flreconstruct --pipeline @falaise:config/snemo/demonstrator/reconstruction/pipeline/1.0.0/flreconstruct.conf --input-file flsimulate.brio --output-file flreconstruct.brio
-/home/ecb/Falaise/build/BuildProducts/bin/flreconstruct --pipeline @falaise:config/snemo/demonstrator/reconstruction/pipeline/1.0.0/flreconstruct.conf --input-file flsimulate.brio --output-file flreconstruct.brio
